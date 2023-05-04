3fb046ab5eff7aa0b41bd86b28f7c1acfee20d05
Lukas Macura <lukas@foresightcyber.com>
2023-05-04 16:11:13 +0200

    Added debug option

--- aidraw.py
+++ aidraw.py
@@ -15,7 +15,9 @@ parser.add_argument('--count', type=int, default=1, help='Number of images to ge
 parser.add_argument('--model', type=str, default='image-alpha-001', help='The ID of the model to use for image generation')
 parser.add_argument('--key', type=str, help='The API key for the OpenAI API. You can use OPENAI_API_KEY env variable too')
 parser.add_argument('--output', type=str, default='images/{sentence}/aidraw-{num}.jpg', required=False, help='Save image[s] to this directory')
+parser.add_argument('--debug', default=False, action='store_true')
 args = parser.parse_args()
+
 
 if not args.key:
     if os.getenv("OPENAI_API_KEY"):
@@ -23,6 +25,14 @@ if not args.key:
     else:
         logging.getLogger().error("You need to set openai key! See aidraw.py -h")
         sys.exit()
+
+if args.debug:
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
+
 
 # set up the API endpoint and headers
 endpoint = 'https://api.openai.com/v1/images/generations'
@@ -42,8 +52,13 @@ data = {
 
 # send the request and parse the response
 response = requests.post(endpoint, headers=headers, data=json.dumps(data))
+if args.debug:
+    print(json.dumps(data, indent=2), file=sys.stderr)
 response.raise_for_status()
 response_data = response.json()
+if args.debug:
+    print(json.dumps(response.json(), indent=2), file=sys.stderr)
+
 
 i = 1
 for urls in response_data['data']:
